ee4300810fdbc0ad225652e5e49c67f02695f209
kupriyanovNik <cucuprianov@icloud.com>
2023-08-12 17:30:40 +0300

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Никита Куприянов on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
